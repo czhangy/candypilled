4ed217a07c77a0586899f737b6943442f6843eb4
Charles Zhang <charleszhangmb@gmail.com>
2026-07-20 12:07:52 -0700

    Cafe Cabin

--- src/lib/games/platinum/locations/cafe-cabin.ts
+++ src/lib/games/platinum/locations/cafe-cabin.ts
@@ -1,9 +1,57 @@
 import { cafeCabin } from '@/lib/games/platinum/maps';
+import { Nature } from '@/lib/static/enums';
 import { Location } from '@/lib/static/types';
 
 const CAFE_CABIN: Location = {
     name: 'Café Cabin',
     map: cafeCabin,
+    battles: [
+        {
+            isOptional: true,
+            trainerClass: 'Waitress',
+            name: 'Kati',
+            team: [
+                {
+                    name: 'Clefairy',
+                    ability: 1,
+                    level: 25,
+                    nature: Nature.Calm,
+                },
+            ],
+            x: 50,
+            y: 33,
+        },
+        {
+            isOptional: true,
+            trainerClass: 'Collector',
+            name: 'Fernando',
+            team: [
+                {
+                    name: 'Heracross',
+                    ability: 1,
+                    level: 25,
+                    nature: Nature.Quirky,
+                },
+            ],
+            x: 25,
+            y: 38.5,
+        },
+        {
+            isOptional: true,
+            trainerClass: 'Collector',
+            name: 'Edwin',
+            team: [
+                {
+                    name: 'Munchlax',
+                    ability: 1,
+                    level: 25,
+                    nature: Nature.Gentle,
+                },
+            ],
+            x: 38,
+            y: 38.5,
+        },
+    ],
 };
 
 export default CAFE_CABIN;
