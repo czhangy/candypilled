82054825f24c3c53624eef6289be4679e336744a
Charles Zhang <charleszhangmb@gmail.com>
2026-07-27 09:21:47 -0700

    Distortion World

--- src/lib/data/platinum/splits/volkner.ts
+++ src/lib/data/platinum/splits/volkner.ts
@@ -1,6 +1,29 @@
+import DISTORTION_WORLD from '@/lib/data/platinum/locations/distortion-world';
 import GALACTIC_HQ from '@/lib/data/platinum/locations/galactic-hq';
+import MT_CORONET from '@/lib/data/platinum/locations/mt-coronet';
+import SPEAR_PILLAR from '@/lib/data/platinum/locations/spear-pillar';
 import { Split } from '@/lib/static/types';
+import LocationHelpers from '@/lib/utils/LocationHelpers';
 
-const VOLKNER: Split = { name: 'Volkner', locations: [GALACTIC_HQ] };
+const VOLKNER: Split = {
+    name: 'Volkner',
+    locations: [
+        GALACTIC_HQ,
+        LocationHelpers.withSubareaOrder(MT_CORONET, [
+            '1F (207)',
+            '2F',
+            '3F',
+            'Exterior',
+            '4F',
+            'Tunnel',
+            '5F',
+            '6F',
+            '1F (211)',
+            '1F (216)',
+        ]),
+        SPEAR_PILLAR,
+        DISTORTION_WORLD,
+    ],
+};
 
 export default VOLKNER;
