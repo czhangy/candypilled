22b3082be6da9dfb9900495a67ebe2e185117c02
Charles Zhang <charleszhangmb@gmail.com>
2026-07-28 09:11:17 -0700

    :obby

--- src/lib/data/platinum/splits/cynthia.ts
+++ src/lib/data/platinum/splits/cynthia.ts
@@ -1,6 +1,10 @@
+import POKEMON_LEAGUE_LOBBY from '@/lib/data/platinum/locations/pokemon-league-lobby';
 import VICTORY_ROAD from '@/lib/data/platinum/locations/victory-road';
 import { Split } from '@/lib/static/types';
 
-const CYNTHIA: Split = { name: 'Cynthia', locations: [VICTORY_ROAD] };
+const CYNTHIA: Split = {
+    name: 'Cynthia',
+    locations: [VICTORY_ROAD, POKEMON_LEAGUE_LOBBY],
+};
 
 export default CYNTHIA;
