90a56e53b1f0ba7bb6616f8365d7db150ce44356
Charles Zhang <charleszhangmb@gmail.com>
2026-08-06 23:04:40 -0700

    Eterna Forest

--- src/lib/data/diamond-pearl/locations/eterna-forest.ts
+++ src/lib/data/diamond-pearl/locations/eterna-forest.ts
@@ -2,6 +2,7 @@ import {
     eternaForestExterior,
     eternaForestInterior,
 } from '@/lib/data/diamond-pearl/maps';
+import { BattleMetadata } from '@/lib/static/enums';
 import { Location } from '@/lib/static/types';
 
 const ETERNA_FOREST: Location = {
@@ -11,6 +12,51 @@ const ETERNA_FOREST: Location = {
             name: 'Interior',
             map: eternaForestInterior,
             encountersKey: 'eterna-forest-interior',
+            battles: [
+                {
+                    metadata: [BattleMetadata.Tag],
+                    customWidth: 72,
+                    battleKey: 'bug-catcher-jack',
+                    x: 34.1,
+                    y: 43.4,
+                },
+                {
+                    metadata: [],
+                    battleKey: 'psychic-f-lindsey',
+                    x: 47.4,
+                    y: 43.4,
+                },
+                {
+                    metadata: [],
+                    battleKey: 'psychic-m-elijah',
+                    x: 53.2,
+                    y: 43.4,
+                },
+                {
+                    metadata: [BattleMetadata.Optional],
+                    battleKey: 'bug-catcher-donald',
+                    x: 64.7,
+                    y: 79.1,
+                },
+                {
+                    metadata: [BattleMetadata.Optional],
+                    battleKey: 'bug-catcher-phillip',
+                    x: 64.7,
+                    y: 87.8,
+                },
+                {
+                    metadata: [BattleMetadata.Optional],
+                    battleKey: 'psychic-m-kody',
+                    x: 75.1,
+                    y: 43.4,
+                },
+                {
+                    metadata: [BattleMetadata.Optional],
+                    battleKey: 'psychic-f-rachael',
+                    x: 78.7,
+                    y: 43.4,
+                },
+            ],
         },
         {
             name: 'Exterior',
