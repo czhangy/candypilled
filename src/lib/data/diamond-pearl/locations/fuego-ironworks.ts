6b2feedbf215c19ce443e4fdc1f63e1648f994b7
Charles Zhang <charleszhangmb@gmail.com>
2026-08-12 22:37:03 -0700

    Route 221

--- src/lib/data/diamond-pearl/locations/fuego-ironworks.ts
+++ src/lib/data/diamond-pearl/locations/fuego-ironworks.ts
@@ -2,6 +2,7 @@ import {
     fuegoIronworksExterior,
     fuegoIronworksInterior,
 } from '@/lib/data/diamond-pearl/maps';
+import { BattleMetadata } from '@/lib/static/enums';
 import { Location } from '@/lib/static/types';
 
 const FUEGO_IRONWORKS: Location = {
@@ -15,6 +16,26 @@ const FUEGO_IRONWORKS: Location = {
         {
             name: 'Interior',
             map: fuegoIronworksInterior,
+            battles: [
+                {
+                    metadata: [BattleMetadata.Optional],
+                    battleKey: 'worker-dillan',
+                    x: 5,
+                    y: 24.9,
+                },
+                {
+                    metadata: [BattleMetadata.Optional],
+                    battleKey: 'worker-holden',
+                    x: 50.2,
+                    y: 20.4,
+                },
+                {
+                    metadata: [BattleMetadata.Optional],
+                    battleKey: 'worker-conrad',
+                    x: 67.9,
+                    y: 91.1,
+                },
+            ],
         },
     ],
 };
