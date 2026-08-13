6206172d8c3e0049759e8a6fe070357e2e5bc8af
Charles Zhang <charleszhangmb@gmail.com>
2026-08-12 22:29:17 -0700

    Route 219

--- src/lib/data/diamond-pearl/locations/route-219.ts
+++ src/lib/data/diamond-pearl/locations/route-219.ts
@@ -1,10 +1,25 @@
 import { route219 } from '@/lib/data/diamond-pearl/maps';
+import { BattleMetadata } from '@/lib/static/enums';
 import { Location } from '@/lib/static/types';
 
 const ROUTE_219: Location = {
     name: 'Route 219',
     map: route219,
     encountersKey: 'sinnoh-route-219',
+    battles: [
+        {
+            metadata: [BattleMetadata.Optional],
+            battleKey: 'tuber-m-trenton',
+            x: 43.8,
+            y: 73.3,
+        },
+        {
+            metadata: [BattleMetadata.Optional],
+            battleKey: 'tuber-f-mariel',
+            x: 51.9,
+            y: 73.3,
+        },
+    ],
 };
 
 export default ROUTE_219;
