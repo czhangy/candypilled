6c2c6db265fe9d973ffa7cfb66357b2cc8832e45
Charles Zhang <charleszhangmb@gmail.com>
2026-08-12 21:40:01 -0700

    Pastoria City

--- src/lib/data/diamond-pearl/locations/pastoria-city.ts
+++ src/lib/data/diamond-pearl/locations/pastoria-city.ts
@@ -1,10 +1,19 @@
 import { pastoriaCity } from '@/lib/data/diamond-pearl/maps';
+import { BattleMetadata } from '@/lib/static/enums';
 import { Location } from '@/lib/static/types';
 
 const PASTORIA_CITY: Location = {
     name: 'Pastoria City',
     map: pastoriaCity,
     encountersKey: 'pastoria-city',
+    battles: [
+        {
+            metadata: [BattleMetadata.Miniboss],
+            battleKey: 'pkmn-trainer-barry-barry-3',
+            x: 74.2,
+            y: 21.2,
+        },
+    ],
 };
 
 export default PASTORIA_CITY;
