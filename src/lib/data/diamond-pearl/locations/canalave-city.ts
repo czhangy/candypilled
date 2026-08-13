7f6c4fcd4a21e1d502c4fde87c7b44ca6d34a5ee
Charles Zhang <charleszhangmb@gmail.com>
2026-08-12 22:49:43 -0700

    Iron Island

--- src/lib/data/diamond-pearl/locations/canalave-city.ts
+++ src/lib/data/diamond-pearl/locations/canalave-city.ts
@@ -1,10 +1,19 @@
 import { canalaveCity } from '@/lib/data/diamond-pearl/maps';
+import { BattleMetadata } from '@/lib/static/enums';
 import { Location } from '@/lib/static/types';
 
 const CANALAVE_CITY: Location = {
     name: 'Canalave City',
     map: canalaveCity,
     encountersKey: 'canalave-city',
+    battles: [
+        {
+            metadata: [BattleMetadata.Miniboss],
+            battleKey: 'pkmn-trainer-barry-barry-4',
+            x: 48.1,
+            y: 30,
+        },
+    ],
 };
 
 export default CANALAVE_CITY;
--- src/lib/data/diamond-pearl/locations/iron-island.ts
+++ src/lib/data/diamond-pearl/locations/iron-island.ts
@@ -7,6 +7,7 @@ import {
     ironIslandB3f,
     ironIslandExterior,
 } from '@/lib/data/diamond-pearl/maps';
+import { BattleMetadata } from '@/lib/static/enums';
 import { Location } from '@/lib/static/types';
 
 const IRON_ISLAND: Location = {
@@ -26,21 +27,108 @@ const IRON_ISLAND: Location = {
             name: 'B1F West',
             map: ironIslandB1fWest,
             encountersKey: 'iron-island-b1f-left',
+            battles: [
+                {
+                    metadata: [BattleMetadata.Optional],
+                    battleKey: 'camper-lawrence',
+                    x: 63,
+                    y: 78.6,
+                },
+            ],
         },
         {
             name: 'B1F East',
             map: ironIslandB1fEast,
             encountersKey: 'iron-island-b1f-right',
+            battles: [
+                {
+                    metadata: [BattleMetadata.Optional],
+                    battleKey: 'picnicker-summer',
+                    x: 75.1,
+                    y: 49.1,
+                },
+            ],
         },
         {
             name: 'B2F East',
             map: ironIslandB2fEast,
             encountersKey: 'iron-island-b2f-right',
+            battles: [
+                {
+                    metadata: [BattleMetadata.Optional],
+                    battleKey: 'worker-willy',
+                    x: 40.3,
+                    y: 23.2,
+                },
+                {
+                    metadata: [BattleMetadata.Optional],
+                    battleKey: 'worker-braden',
+                    x: 9.7,
+                    y: 67.6,
+                },
+            ],
         },
         {
             name: 'B2F West',
             map: ironIslandB2fWest,
             encountersKey: 'iron-island-b2f-left',
+            battles: [
+                {
+                    metadata: [BattleMetadata.Optional],
+                    battleKey: 'hiker-damon',
+                    x: 64.4,
+                    y: 6.3,
+                },
+                {
+                    metadata: [BattleMetadata.Optional],
+                    battleKey: 'hiker-maurice',
+                    x: 52.5,
+                    y: 9.6,
+                },
+                {
+                    metadata: [BattleMetadata.Optional],
+                    battleKey: 'black-belt-kendal',
+                    x: 88.2,
+                    y: 33.5,
+                },
+                {
+                    metadata: [BattleMetadata.Optional],
+                    battleKey: 'battle-girl-tyler',
+                    x: 88.2,
+                    y: 38.1,
+                },
+                {
+                    metadata: [BattleMetadata.Optional],
+                    battleKey: 'worker-brendon',
+                    x: 38.3,
+                    y: 44.9,
+                },
+                {
+                    metadata: [BattleMetadata.Optional],
+                    battleKey: 'worker-quentin',
+                    x: 35.7,
+                    y: 49.8,
+                },
+                {
+                    metadata: [BattleMetadata.Optional],
+                    battleKey: 'ace-trainer-m-jonah',
+                    x: 57.1,
+                    y: 53.6,
+                },
+                {
+                    metadata: [BattleMetadata.Optional],
+                    battleKey: 'ace-trainer-f-brenda',
+                    x: 66.9,
+                    y: 53.6,
+                },
+                {
+                    metadata: [BattleMetadata.Optional, BattleMetadata.Tag],
+                    customHeight: 40,
+                    battleKey: 'galactic-grunt-m-18',
+                    x: 47.7,
+                    y: 69.5,
+                },
+            ],
         },
         {
             name: 'B3F',
