438a690c348f99df3f69aa444fef8ecf4058e679
Charles Zhang <charleszhangmb@gmail.com>
2026-08-13 20:25:53 -0700

    Spear Pillar

--- src/lib/data/diamond-pearl/locations/spear-pillar.ts
+++ src/lib/data/diamond-pearl/locations/spear-pillar.ts
@@ -1,9 +1,32 @@
 import { spearPillar } from '@/lib/data/diamond-pearl/maps';
+import { BattleMetadata } from '@/lib/static/enums';
 import { Location } from '@/lib/static/types';
 
 const SPEAR_PILLAR: Location = {
     name: 'Spear Pillar',
     map: spearPillar,
+    battles: [
+        {
+            metadata: [BattleMetadata.Double],
+            customWidth: 54,
+            battleKey: 'galactic-grunt-f-14',
+            x: 49.3,
+            y: 75.7,
+        },
+        {
+            metadata: [BattleMetadata.Miniboss, BattleMetadata.Tag],
+            customWidth: 84,
+            battleKey: 'commander-jupiter-jupiter-2',
+            x: 49.8,
+            y: 48.3,
+        },
+        {
+            metadata: [BattleMetadata.Miniboss],
+            battleKey: 'galactic-boss-cyrus-cyrus-2',
+            x: 49.6,
+            y: 41.5,
+        },
+    ],
 };
 
 export default SPEAR_PILLAR;
