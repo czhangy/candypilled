04dfad0a0224c6a3471cf2f35270c62185db34ad
Charles Zhang <charleszhangmb@gmail.com>
2026-08-15 14:00:46 -0700

    Sunnyshore Gym:

--- src/lib/data/diamond-pearl/locations/sunyshore-gym.ts
+++ src/lib/data/diamond-pearl/locations/sunyshore-gym.ts
@@ -3,7 +3,7 @@ import {
     sunyshoreGymRoom2,
     sunyshoreGymRoom3,
 } from '@/lib/data/diamond-pearl/maps';
-import { MapAnchor } from '@/lib/static/enums';
+import { BattleMetadata, MapAnchor } from '@/lib/static/enums';
 import { Location } from '@/lib/static/types';
 
 const SUNYSHORE_GYM: Location = {
@@ -13,16 +13,76 @@ const SUNYSHORE_GYM: Location = {
             name: 'Room 1',
             map: sunyshoreGymRoom1,
             mapAnchor: MapAnchor.Center,
+            battles: [
+                {
+                    metadata: [],
+                    battleKey: 'school-kid-f-tiera',
+                    x: 36.6,
+                    y: 24.2,
+                },
+            ],
         },
         {
             name: 'Room 2',
             map: sunyshoreGymRoom2,
             mapAnchor: MapAnchor.Center,
+            battles: [
+                {
+                    metadata: [],
+                    battleKey: 'school-kid-m-forrest',
+                    x: 21.6,
+                    y: 31.3,
+                },
+                {
+                    metadata: [],
+                    battleKey: 'guitarist-jerry',
+                    x: 83.9,
+                    y: 55.4,
+                },
+                {
+                    metadata: [BattleMetadata.Optional],
+                    battleKey: 'poke-kid-meghan',
+                    x: 71.4,
+                    y: 17.9,
+                },
+            ],
         },
         {
             name: 'Room 3',
             map: sunyshoreGymRoom3,
             mapAnchor: MapAnchor.Center,
+            battles: [
+                {
+                    metadata: [BattleMetadata.Optional],
+                    battleKey: 'guitarist-lonnie',
+                    x: 73.4,
+                    y: 90,
+                },
+                {
+                    metadata: [],
+                    battleKey: 'ace-trainer-f-destiny',
+                    x: 16.1,
+                    y: 89.7,
+                },
+                {
+                    metadata: [],
+                    battleKey: 'guitarist-preston',
+                    x: 11.3,
+                    y: 12.6,
+                },
+                {
+                    metadata: [],
+                    battleKey: 'ace-trainer-m-zachery',
+                    x: 97.3,
+                    y: 41.7,
+                },
+                {
+                    metadata: [BattleMetadata.Boss],
+                    battleKey: 'leader-volkner-volkner',
+                    x: 49.6,
+                    y: 12.6,
+                },
+            ],
         },
     ],
 };
