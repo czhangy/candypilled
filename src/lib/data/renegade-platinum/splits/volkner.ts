988e90a1ae5361c8501771b35e992eb4c47bee32
Charles Zhang <charleszhangmb@gmail.com>
2026-08-29 10:10:46 -0400

    Mt Coronet

--- src/lib/data/renegade-platinum/splits/volkner.ts
+++ src/lib/data/renegade-platinum/splits/volkner.ts
@@ -1,10 +1,28 @@
 import GALACTIC_HQ from '@/lib/data/renegade-platinum/locations/galactic-hq';
+import MT_CORONET from '@/lib/data/renegade-platinum/locations/mt-coronet';
 import ROUTE_216 from '@/lib/data/renegade-platinum/locations/route-216';
 import { Split } from '@/lib/static/types';
+import LocationHelpers from '@/lib/utils/LocationHelpers';
 
 const VOLKNER: Split = {
     name: 'Volkner',
-    locations: [ROUTE_216, GALACTIC_HQ],
+    locations: [
+        ROUTE_216,
+        GALACTIC_HQ,
+        LocationHelpers.withSubareaOrder(MT_CORONET, [
+            '1F (207)',
+            '2F',
+            '3F',
+            'Exterior',
+            '4F',
+            'Summit',
+            'Tunnel',
+            '5F',
+            '6F',
+            '1F (211)',
+            '1F (216)',
+        ]),
+    ],
     saveCondition: { type: 'badge', bit: 7 },
 };
 
