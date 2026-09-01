721971f65ee675976f6d956f8d5d3cce0185343c
Charles Zhang <charleszhangmb@gmail.com>
2026-08-31 17:35:32 -0700

    Improve loading view (#392)
    * Spinner
    
    * Rehydrate

---
+++ src/components/common/Spinner/Spinner.tsx
@@ -0,0 +1,11 @@
+import styles from './Spinner.module.scss';
+
+const Spinner: React.FC = () => {
+    // -------------------------------------------------------------------------
+    // MARKUP
+    // -------------------------------------------------------------------------
+
+    return <div className={styles.spinner} />;
+};
+
+export default Spinner;
